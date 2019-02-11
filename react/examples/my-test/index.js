1e0bbb87abb823ad44688544b1623bd373f002be
Pievi <r1jo1jo1@gmail.com>
2019-02-11 20:42:52 +0200

    Added handel multiple renders

--- react/examples/my-test/index.js
+++ react/examples/my-test/index.js
@@ -1,10 +1,21 @@
 var SelectPrice = React.createClass({
     getInitialState: function () {
-        return {
-            clickCount: 0,
-            qty: 1000,
-            info: 1,
-        };
+
+        test = this;
+        
+        let props = this.props;
+        let state = this.props.state;
+        
+        if (state) {
+            return state;
+        }
+        
+        return props;
+    },
+    componentDidMount() {
+        this.setState({
+            containerId: ReactDOM.findDOMNode(this).parentNode.getAttribute("id")
+        });
     },
     handleClick: function () {
         this.setState(function (state) {
@@ -12,42 +23,39 @@ var SelectPrice = React.createClass({
         });
     },
     updateInputValue: function (e, id) {
-
         this.setState({
             [id] : e
         });
     },
     displayForm: function (e, data) {
 
-        console.log(this);
-        console.log(e);
-        console.log(data);
+        // console.log(this);
+        // console.log(e);
+        // console.log(data);
 
         ReactDOM.render(
             <DisplayForm
-                state='' // TODO: How tow pass getInitialState?
+                state={this.state}
             />,
-            document.getElementById('container')
+            document.getElementById(this.state.containerId)
         );
     },
     render: function () {
 
-        let props = this.props;
-
         return (
             <div>
-                <img src={props.logo} />
+                <img src={this.state.logo} />
 
-                <h1>{props.name}</h1>
+                <h1>{this.state.name}</h1>
                 <h2 onClick={this.handleClick}>Click me! Number of clicks: {this.state.clickCount}</h2>
 
-                <p className='price'>{props.price}€</p>
-                <p className='sale'>{props.sale}€</p>
+                <p className='price'>{this.state.price}€</p>
+                <p className='sale'>{this.state.sale}€</p>
 
                 <p>Qty</p>
-                <input id="qty" className='input-line' type='text' onChange={e=>this.updateInputValue(e.target.value, "qty")}></input>
+                <input className='input-line' type='text' value={this.state.qty} onChange={e=>this.updateInputValue(e.target.value, "qty")}></input>
                 <p>Info</p>
-                <input id='info' className='input-line' type='text' onChange={e=>this.updateInputValue(e.target.value, "info")}></input>
+                <input className='input-line' type='text' value={this.state.info} onChange={e=>this.updateInputValue(e.target.value, "info")}></input>
 
                 <button className='input-line' onClick={this.displayForm.bind(this, 1)}>sale price</button>
                 <button className='input-line' onClick={this.displayForm.bind(this, 0)}>orginal price</button>
@@ -58,14 +66,100 @@ var SelectPrice = React.createClass({
 
 
 var DisplayForm = React.createClass({
+    getInitialState: function () {
+        let state = this.props.state;
+        // console.log(state);
+        return state;
+    },
+    updateInputValue: function (e, id) {
+        this.setState({
+            [id] : e
+        });
+    },
+    displayForm: function (e, data) {
+        if (e === 1) {
+            ReactDOM.render(
+                <DisplaySummary
+                    state={this.state}
+                />,
+                document.getElementById(this.state.containerId)
+            );
+        } else {
+            ReactDOM.render(
+                <SelectPrice
+                    state={this.state}
+                />,
+                document.getElementById(this.state.containerId)
+            );
+        }
+    },
     render: function () {
         return (
             <div>
                 <p>Firstname</p>
-                <input className='input-line' type='text'></input>
+                <input className='input-line' type='text' value={this.state.firstName} onChange={e=>this.updateInputValue(e.target.value, "firstName")}></input>
                 <p>Lastname</p>
-                <input className='input-line' type='text'></input>
-                <button className='input-line' onClick={this.selectPrice}>Back</button>
+                <input className='input-line' type='text' value={this.state.lastName} onChange={e=>this.updateInputValue(e.target.value, "lastName")}></input>
+                
+                <button className='input-line' onClick={this.displayForm.bind(this, 1)}>Next</button>
+                <button className='input-line' onClick={this.displayForm.bind(this, 0)}>Back</button>
+            </div>
+        );
+    }
+});
+
+var DisplaySummary = React.createClass({
+    getInitialState: function () {
+        let state = this.props.state;
+        // console.log(state);
+        return state;
+    },
+    displayForm: function (e, data) {
+        if (e === 1) {
+            ReactDOM.render(
+                <DisplayThankYou
+                    state={this.state}
+                />,
+                document.getElementById(this.state.containerId)
+            );
+        } else {
+            ReactDOM.render(
+                <DisplayForm
+                    state={this.state}
+                />,
+                document.getElementById(this.state.containerId)
+            );
+        }
+    },
+    render: function () {
+        return (
+            <div>
+                <p>Sale price: {this.state.price}</p>
+                <p>Orginal price: {this.state.sale}</p>
+                <p>Qty: {this.state.qty}</p>
+                <p>Info: {this.state.info}</p>
+                <p>Firstname: {this.state.firstName}</p>
+                <p>Lastname: {this.state.lastName}</p>
+                
+                
+                <button className='input-line' onClick={this.displayForm.bind(this, 1)}>Submit</button>
+                <button className='input-line' onClick={this.displayForm.bind(this, 0)}>Back</button>
+            </div>
+        );
+    }
+});
+
+var DisplayThankYou = React.createClass({
+    getInitialState: function () {
+        let state = this.props.state;
+        // console.log(state);
+        return state;
+    },
+    render: function () {
+        return (
+            <div>
+                <p>Thank You!!!</p>
+                <p>Display response message from server here</p>
             </div>
         );
     }
@@ -79,4 +173,15 @@ ReactDOM.render(
         name='Rune Scimitar'
     />,
     document.getElementById('container')
+);
+
+
+ReactDOM.render(
+    <SelectPrice
+        price='200'
+        sale='190'
+        logo='https://vignette.wikia.nocookie.net/2007scape/images/9/97/Dragon_scimitar_detail.png/revision/latest?cb=20170118191039'
+        name='Dragon Scimitar'
+    />,
+    document.getElementById('container2')
 );
